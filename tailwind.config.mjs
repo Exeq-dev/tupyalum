e6dcf716885696d88b3bee81e0bb7b55f48b4b38
Exeq-dev <albornozexequiel01@gmail.com>
2024-10-05 00:31:27 -0300

    Inicio proyecto

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
+    typography,
+    aspectRatio,
+  ],
+};
